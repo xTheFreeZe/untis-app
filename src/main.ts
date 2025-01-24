cb65f622c8f32cf2821ccdcf9de141e2a0e3125d
xTheFreeZe <marwin.eder@gmx.de>
2025-01-25 00:16:13 +0100

    First

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { WebUntis } from "webuntis";
+import { configDotenv } from "dotenv";
+configDotenv();
+
+const school = process.env.SCHOOL ?? "";
+const username = process.env.NAME ?? "";
+const password = process.env.PASSWORD ?? "";
+const server = process.env.SERVER ?? "";
+
+const untis = new WebUntis(school, username, password, server);
+
+const main = async () => {
+  try {
+    await untis.login();
+    console.log("[SUCESS] Logged in");
+  } catch (error) {
+    console.error(error);
+  }
+
+  const yersterday = new Date();
+  yersterday.setDate(yersterday.getDate() - 2); // -2 für Vorgestern
+  const timetable = await untis.getOwnTimetableFor(yersterday);
+  const classes = timetable.map((lesson) => lesson.su[0].longname);
+
+  console.log(classes);
+
+  try {
+    await untis.logout();
+    console.log("[SUCESS] Logged out");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+main();
