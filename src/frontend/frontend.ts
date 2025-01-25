3eda7fe230c54cfe0ec9dc8039060634a81956c0
xTheFreeZe <marwin.eder@gmx.de>
2025-01-25 15:28:20 +0100

    Fixed code

--- src/frontend/frontend.ts
+++ src/frontend/frontend.ts
@@ -1,5 +1,5 @@
-import { Lesson } from "webuntis"
-import { TimetableData } from "../api/lesson/getTimetable"
+import { Lesson } from "webuntis";
+import { TimetableData } from "../api/lesson/getTimetable";
 
 enum boxCharTypes {
   Vertical = 0,
@@ -9,48 +9,52 @@ enum boxCharTypes {
   Lowerleft,
   Lowerright,
 }
-const type = boxCharTypes
+const type = boxCharTypes;
 
-const boxChars = ["║", "═", "╔", "╗", "╚", "╝"]
+const boxChars = ["║", "═", "╔", "╗", "╚", "╝"];
 
 function createAsciiBox(lessons: string[]): string {
-  const maxLineLength = Math.max(...lessons.map((line) => line.length))
-  const boxWidth = maxLineLength + 2
+  const maxLineLength = Math.max(...lessons.map((line) => line.length));
+  const boxWidth = maxLineLength + 2;
 
   const topBorder = `${boxChars[type.Upperleft]}${boxChars[
     type.Horizontal
-  ].repeat(boxWidth)}${boxChars[type.Upperright]}`
+  ].repeat(boxWidth)}${boxChars[type.Upperright]}`;
 
   const bottom = `${boxChars[type.Lowerleft]}${boxChars[type.Horizontal].repeat(
-    boxWidth
-  )}${boxChars[type.Lowerright]}`
+    boxWidth,
+  )}${boxChars[type.Lowerright]}`;
 
   const lessonLines = lessons.map((line) => {
-    const padding = " ".repeat(boxWidth - line.length)
+    const padding = " ".repeat(boxWidth - line.length);
     return `${boxChars[type.Vertical]}${line}${padding}${
       boxChars[type.Vertical]
-    }`
-  })
+    }`;
+  });
 
-  return [topBorder, ...lessonLines, bottom].join("\n")
+  return [topBorder, ...lessonLines, bottom].join("\n");
 }
 
 export function userInterface(data: TimetableData) {
-  var lessonText: string[]
+  var lessonText: string[];
 
-  lessonText = [""]
-  for (var i = 0; i < data.lessons.length; i++) {
-    if ((i = 0)) lessonText = [`Lesson ${i + 1}: ${data.lessons[i].subject}`]
-    lessonText.push(` - from: ${data.lessons[i].startTime}`)
-    lessonText.push(` - until: ${data.lessons[i].endTime}`)
+  console.log("Yo");
+
+  lessonText = [""];
+  for (let i = 0; i < data.lessons.length; i++) {
+    if (i == 0) {
+      lessonText = [`Lesson ${i + 1}: ${data.lessons[i].subject}`];
+    }
+    lessonText.push(` - from: ${data.lessons[i].startTime}`);
+    lessonText.push(` - until: ${data.lessons[i].endTime}`);
     if (data.lessons[i].orignalTeacher != undefined) {
       lessonText.push(
-        ` - teacher: (${data.lessons[i].orignalTeacher}) ${data.lessons[i].teacher}`
-      )
+        ` - teacher: (${data.lessons[i].orignalTeacher}) ${data.lessons[i].teacher}`,
+      );
     } else {
-      lessonText.push(` - teacher: ${data.lessons[i].teacher}`)
+      lessonText.push(` - teacher: ${data.lessons[i].teacher}`);
     }
   }
 
-  console.log(createAsciiBox(lessonText))
+  console.log(createAsciiBox(lessonText));
 }
